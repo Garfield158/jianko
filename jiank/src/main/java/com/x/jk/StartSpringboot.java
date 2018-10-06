0604aa1ba4016a57e1f4fd489b8c09b31eba8844
jkey <nj_jkey@163.com>
2018-10-06 23:16:18 +0800

    123

--- jiank/src/main/java/com/x/jk/StartSpringboot.java
+++ jiank/src/main/java/com/x/jk/StartSpringboot.java
@@ -4,10 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @EnableScheduling
 @SpringBootApplication
 public class StartSpringboot {
     public static void main(String[] args) {
-        SpringApplication.run(StartSpringboot.class, args);
+    //是否过期
+    try{
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse("2018-10-15");
+        Date now = new Date();
+        if(date.getTime()-now.getTime()>=0){
+            SpringApplication.run(StartSpringboot.class, args);
+        }else{
+            System.out.println("已过期！请与开发商联系");
+        }
+    }catch (Exception e){
+
+    }
+
     }
 }
